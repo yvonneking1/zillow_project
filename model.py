aedf7697063d43dad6ac956a1392b62c9e3054c2
Yvonne K <yvonneking017@gmail.com>
2020-04-12 15:02:28 -0500

    Tweaked code for plotting

--- model.py
+++ model.py
@@ -57,12 +57,12 @@ def plot_residuals(actual, predicted, plot_title='Predicted vs Actual'):
     plt.ylabel('residual ($y - \hat{y}$)')
     plt.xlabel('actual value ($y$)')
     plt.title(plot_title)
-    return plt.gca()
+#     return plt.gca()
     
 
 def plot_all_residuals(predictions):
     plt.suptitle('Residuals for all Models\n', fontsize=18)
-    plt.figure(figsize=(16, 16))
+    plt.figure(figsize=(16, 14))
     residual_bl = predictions.actual - predictions.baseline
     residual_all3_lm = predictions.actual - predictions.Sqft_nbth_nbd_lm
     residual_sqft_lm = predictions.actual - predictions.sqft_lm
@@ -147,17 +147,75 @@ def plot_test_residuals(df, test_predictions):
     residual1 = df.actual - test_predictions
     residual2 = df.actual - df.baseline
 
-    plt.figure(figsize=(16, 16))
+    plt.figure(figsize=(16, 14))
 
-    plt.subplot(2, 1, 1)
     plt.hlines(0, df.actual.min(), df.actual.max(), ls=':')
-    plt.scatter(df.actual, residual1, c="#006AFF", alpha=0.5)
+    plt.scatter(df.actual, residual1, c="#006AFF", alpha=0.5, label="Model")
+    plt.scatter(df.actual, residual2, c="#f2af34ff", alpha=0.5, label="Baseline")
     plt.ylabel('residual ($y - \hat{y}$)')
     plt.xlabel('actual value ($y$)')
-    plt.title("Residuals for Test predictions")
+    plt.legend()
+    plt.title("Residuals for Test predictions and baseline", fontsize=18)
+    
+    
+    
+def plot_all_residuals_1(predictions):
+    plt.suptitle('Residuals for all Models\n', fontsize=18)
+    plt.figure(figsize=(16, 14))
+    residual_bl = predictions.actual - predictions.baseline
+    residual_all3_lm = predictions.actual - predictions.Sqft_nbth_nbd_lm
+    residual_sqft_lm = predictions.actual - predictions.sqft_lm
+    residual_n_bed_lm = predictions.actual - predictions.n_bed_lm
+    residual_n_bath_lm= predictions.actual - predictions.n_bath_lm
+    residual_kbest = predictions.actual - predictions.lm_kbest
 
-    plt.subplot(2, 1, 2)
-    plt.scatter(df.actual, residual2, c="#f2af34ff", alpha=0.5)
+    #All 3 features lm
+    plt.subplot(3, 2, 1)
+    plt.hlines(0, predictions.actual.min(), predictions.actual.max(), ls=':')
+    plt.scatter(predictions.actual, residual_all3_lm, c="#006AFF", alpha=0.5, label="Model: Sqft, n_bath, n_bed")
+    plt.scatter(predictions.actual, residual_bl, c="#f2af34ff", label="Baseline")
+    plt.legend()
     plt.ylabel('residual ($y - \hat{y}$)')
     plt.xlabel('actual value ($y$)')
-    plt.title("Residuals for Baseline")
+    plt.title("Model for: SqFt, baths, beds")
+
+    # K best fit model
+    plt.subplot(3, 2, 5)
+    plt.hlines(0, predictions.actual.min(), predictions.actual.max(), ls=':')
+    plt.scatter(predictions.actual, residual_kbest, c="#006AFF", alpha=0.5, label="Model: Kbest")
+    plt.scatter(predictions.actual, residual_bl, c="#f2af34ff", alpha=0.5, label="Baseline")
+    plt.legend()
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title("Model for: Features selected by KBest") 
+    
+    # SqFt lm
+    plt.subplot(3, 2, 2)
+    plt.hlines(0, predictions.actual.min(), predictions.actual.max(), ls=':')
+    plt.scatter(predictions.actual, residual_sqft_lm, c="#006AFF", alpha=0.5, label="Model: Sqft")
+    plt.scatter(predictions.actual, residual_bl, c="#f2af34ff", alpha=0.5, label="Baseline")
+    plt.legend()
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title("Model for: SqFt")
+
+    # Number of beds lm
+    plt.subplot(3, 2, 3)
+    plt.hlines(0, predictions.actual.min(), predictions.actual.max(), ls=':')
+    plt.scatter(predictions.actual, residual_n_bed_lm, c="#006AFF", alpha=0.5, label="Model: n_beds")
+    plt.scatter(predictions.actual, residual_bl, c="#f2af34ff", alpha=0.5, label="Baseline")
+    plt.legend()
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title("Model for: Bedroom count")
+
+    # Number of bath lm
+    plt.subplot(3, 2, 4)
+    plt.hlines(0, predictions.actual.min(), predictions.actual.max(), ls=':')
+    plt.scatter(predictions.actual, residual_n_bath_lm, c="#006AFF", alpha=0.5, label="Model: n_bath")
+    plt.scatter(predictions.actual, residual_bl, c="#f2af34ff", alpha=0.5, label="Baseline")
+    plt.legend()
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title("Model for: Bathroom count")
+    
